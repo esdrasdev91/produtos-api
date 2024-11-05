75d31988e8bbf45155686736f0450b623d519124
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-11-05 12:11:33 -0300

    Create class FakeApiService

---
+++ src/main/java/com/esdrasdev/produtos_api/business/service/FakeApiService.java
@@ -0,0 +1,42 @@
+package com.esdrasdev.produtos_api.business.service;
+
+import com.esdrasdev.produtos_api.business.converter.ProdutoConverter;
+import com.esdrasdev.produtos_api.dto.ProductsDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FakeApiService {
+
+    private final FakeApiClient cliente;
+    private final ProdutoConverter converter;
+    private final ProdutoService produtoService;
+
+    @NotificacaoErro
+    public List<ProductsDTO> buscaProdutos() {
+        try {
+
+
+            List<ProductsDTO> dto = cliente.buscaListaProdutos();
+            dto.forEach(produto -> {
+                        Boolean retorno = produtoService.existsPorNome(produto.getNome());
+                        if (retorno.equals(false)) {
+                            produtoService.salvaProdutos(converter.toEntity(produto));
+                        } else {
+                            throw new ConflictException("Produto já existente no banco de dados " + produto.getNome());
+                        }
+                    }
+
+            );
+            return produtoService.buscaTodosProdutos();
+        }catch (ConflictException e) {
+            throw new ConflictException(e.getMessage());
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao buscar e gravar produtos no banco de dados");
+        }
+    }
+
+}
