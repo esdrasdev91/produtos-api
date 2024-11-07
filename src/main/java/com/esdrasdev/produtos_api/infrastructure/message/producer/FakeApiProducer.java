50d8dbf168d8687ac07d4c679a7ba2aa5525e1a0
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-11-07 10:09:01 -0300

    create new producer kafka

---
+++ src/main/java/com/esdrasdev/produtos_api/infrastructure/message/producer/FakeApiProducer.java
@@ -0,0 +1,31 @@
+package com.esdrasdev.produtos_api.infrastructure.message.producer;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Component
+public class FakeApiProducer {
+
+    @Value("${topico.fake-api.producer.nome}")
+    private String topico;
+
+    @Autowired
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public FakeApiProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void enviaRespostaCadastroProdutos(final String mensagem) {
+        try{
+            kafkaTemplate.send(topico, mensagem);
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao produzir mensagem do kafka");
+        }
+    }
+
+}
